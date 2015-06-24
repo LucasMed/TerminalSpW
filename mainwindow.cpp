dae93a897c0e5bcda17543f191ee52bdff1f7702
LucasMed <elotrobo69@hotmail.com>
2015-06-24 17:40:47 -0300

    Modificacion de la GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->actionExit->setEnabled(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+
+}
+
+void MainWindow::on_actionHelp_triggered()
+{
+    QMessageBox::about(this, tr("About Simple Terminal"),
+                       tr("The <b>Simple Terminal</b> example demonstrates how to "
+                          "use the Qt Serial Port module in modern GUI applications "
+                          "using Qt, with a menu bar, toolbars, and a status bar."));
 }
